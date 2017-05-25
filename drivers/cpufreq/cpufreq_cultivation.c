66545dafd7453a013eff794b666a44bd0e8f216e
Dabug123 <clarkbrunswick55@gmail.com>
2017-05-25 15:18:23 -0400

    gov:cultivation:edit values for performance increase

--- drivers/cpufreq/cpufreq_cultivation.c
+++ drivers/cpufreq/cpufreq_cultivation.c
@@ -65,7 +65,7 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 90
+#define DEFAULT_TARGET_LOAD 85
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
@@ -92,7 +92,7 @@ struct cpufreq_cultivation_tunables {
 	 * The minimum amount of time to spend at a frequency before we can ramp
 	 * down.
 	 */
-#define DEFAULT_MIN_SAMPLE_TIME (80 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (85 * USEC_PER_MSEC)
 	unsigned long min_sample_time;
 
 	/*
