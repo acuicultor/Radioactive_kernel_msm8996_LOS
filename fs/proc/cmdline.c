1b401a7971995de1e49a9e49c198bcd680e85bd4
Noxxx <apowertje@hotmail.com>
2017-06-22 20:27:48 -0700

    Update SafetyNet bypass
    Credits: jcadduono

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,11 +4,11 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
-static char new_command_line[COMMAND_LINE_SIZE];
+static char proc_cmdline[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", new_command_line);
+	seq_printf(m, "%s\n", proc_cmdline);
 	return 0;
 }
 
@@ -48,13 +48,27 @@ static void remove_safetynet_flags(char *cmd)
 
 static int __init proc_cmdline_init(void)
 {
-	strcpy(new_command_line, saved_command_line);
+	char *a1, *a2;
+
+	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
+	if (a1) {
+		a1 = strchr(a1, '=');
+		a2 = strchr(a1, ' ');
+		if (!a2) /* last argument on the cmdline */
+			a2 = "";
+
+		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
+			  (int)(a1 - saved_command_line + 1),
+			  saved_command_line, a2);
+	} else {
+		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
 
 	/*
 	 * Remove various flags from command line seen by userspace in order to
 	 * pass SafetyNet CTS check.
 	 */
-	remove_safetynet_flags(new_command_line);
+	remove_safetynet_flags(proc_cmdline);
+	}
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
